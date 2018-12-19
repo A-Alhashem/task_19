13adbb8f4585518db128abe4586221c23c0d212f
aishabn <abn.226@gmail.com>
2018-12-19 18:54:31 +0300

    task_19 aziz aisha

--- api/serializers.py
+++ api/serializers.py
@@ -2,33 +2,67 @@ from rest_framework import serializers
 from restaurants.models import Restaurant
 
 class RestaurantListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Restaurant
-        fields = [
-            'name',
-            'opening_time',
-            'closing_time',
-            ]
+	detail = serializers.HyperlinkedIdentityField(
+		view_name = "api-detail",
+		lookup_field = "id",
+		lookup_url_kwarg = "restaurant_id"
+		)
+
+	update = serializers.HyperlinkedIdentityField(
+		view_name = "api-update",
+		lookup_field = "id",
+		lookup_url_kwarg = "restaurant_id"
+		)
+
+	delete = serializers.HyperlinkedIdentityField(
+		view_name = "api-delete",
+		lookup_field = "id",
+		lookup_url_kwarg = "restaurant_id"
+		)
+
+
+	class Meta:
+		model = Restaurant
+		fields = [
+			'name',
+			'opening_time',
+			'closing_time',
+			'detail',
+			'update',
+			'delete',
+			]
 
 
 class RestaurantDetailSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Restaurant
-        fields = [
-            'id',
-            'owner',
-            'name',
-            'description',
-            'opening_time',
-            'closing_time',
-            ]
+	update = serializers.HyperlinkedIdentityField(
+		view_name = "api-update",
+		lookup_field = "id",
+		lookup_url_kwarg = "restaurant_id"
+		)
+
+	delete = serializers.HyperlinkedIdentityField(
+		view_name = "api-delete",
+		lookup_field = "id",
+		lookup_url_kwarg = "restaurant_id"
+		)
+	class Meta:
+		model = Restaurant
+		fields = [
+			'id',
+			'owner',
+			'name',
+			'description',
+			'opening_time',
+			'closing_time',
+			'update', 'delete',
+			]
 
 class RestaurantCreateUpdateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Restaurant
-        fields = [
-            'name',
-            'description',
-            'opening_time',
-            'closing_time',
-            ]
+	class Meta:
+		model = Restaurant
+		fields = [
+			'name',
+			'description',
+			'opening_time',
+			'closing_time',
+			]
